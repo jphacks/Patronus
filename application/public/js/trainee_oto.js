b98fb58d8c33df7afc4f844c26cbb525aa8bfca9
KeisukeSugiura <yosshii4081umeboshi@yahoo.co.jp>
2016-11-05 22:56:46 +0900

    wrote excutions on events in trainee

--- application/public/js/trainee_oto.js
+++ application/public/js/trainee_oto.js
@@ -1,6 +1,6 @@
 'use strict'
 
-//const {ipcRenderer} = require('electron');
+const {ipcRenderer} = require('electron');
 var screenWidth = null;
 var screenHeight = null;
 var patronusManager = null;
@@ -16,7 +16,17 @@ window.onload = function(){
 	screenHeight = window.parent.screen.height;
 
 	guiderVideoElement = document.createElement('video');
+	guiderVideoCanvasElement = document.createElement('canvas');
 	guiderCanvasElement = document.createElement('canvas');
+
+	guiderVideoElement.width = screenWidth;
+	guiderVideoElement.height = screenHeight;
+	guiderVideoElement.style.width = String(screenWidth)+'px';
+	guiderVideoElement.style.height = String(screenHeight)+'px';
+	guiderVideoElement.style.backgroundColor = 'rgba(0,0,0,0)'
+	guiderVideoElement.id = 'remote_video';
+	guiderVideoElement.style.position="fixed";
+	guiderVideoElement.style.zIndex = 0;
 
 	guiderVideoElement.width = screenWidth;
 	guiderVideoElement.height = screenHeight;
@@ -36,17 +46,50 @@ window.onload = function(){
 	guiderCanvasElement.style.position="fixed";
 	guiderCanvasElement.style.zIndex = 1;
 
-	document.body.appendChild(guiderVideoElement);
+	//document.body.appendChild(guiderVideoElement);
+	document.body.appendChild(guiderVideoCanvasElement);
 	document.body.appendChild(guiderCanvasElement);
 
-	PatronusManager.prototype.onPeerOpened= function(id){
-		console.log('override!!');
-		
+	guiderVideoElement.addEventListener('timeupdate',()=>{
+		//ここで画像処理をする
+		const guiderVideoCanvasElementContext = guiderVideoCanvasElement.getContext('2d');
+		guiderVideoCanvasElement.drawImage(guiderVideoElement, 0, 0, guiderVideoElement.width, guiderVideoElement.height); 
+	});
 
+	PatronusManager.prototype.onPeerOpened = function(id){
+		console.log('override!!');
+		ipcRenderer.send('create_guider_window',{peerId:id,width:screenWidth,height:screenHeight});
 	}
+
+	PatronusManager.prototype.onDataConnectionOpend = function(conn){
+		loopGetScreenShotAndSync();
+	}
+
+	patronusManager.prototype.onDataReceived = function(data){
+		//ビデオサイズの交換？
+		//clickevent表記？
+	}
+
 
 	patronusManager = new PatronusManager(SKYWAY_API_KEY);
 	patronusManager.setRemoteVideoElement(guiderVideoElement);
 
 
 }
+
+
+function loopGetScreenShotAndSync(){
+	ipcRenderer.send('get_screenshot',{})
+}
+
+
+ipcRenderer.on('re_get_screenshot',(event,arg)=>{
+	console.log(arg);
+	//url化必要そう
+	patronusManager.broadcastData2AllConnection({act:"sync_screenshot",img:arg});
+	setTimeout(loopGetScreenShotAndSync,1000);
+});
+
+
+
+
