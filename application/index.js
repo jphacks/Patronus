ccaeafbbb95b256a9347f24cb9a07d81764db671
大部達也 <tobe@toralab.org>
2016-11-06 10:15:33 +0900

    no message

--- application/index.js
+++ application/index.js
@@ -1,56 +1,62 @@
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
 
+const electron = require('electron');
+
+const defaultMenu = require('electron-default-menu');
+
+const { app, BrowserWindow, Menu, shell } = electron;
 
 const Connector = require('./modules/connector.js');
+
 let parentWindows = {};
 let childWindows = {};
 const shareWindow = require('./modules/shareWindow.js')(parentWindows, childWindows);
+
+const window_builder = require(path.join(__dirname, 'modules', 'windowBuilder.js'));
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-
 let role = {role: null};
 
-function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        width: 600,
-        height: 400,
-        center: true,
-        titleBarStyle: 'hidden-inset',
-        movable: true
-    });
-
-    // and load the index.html of the app.
-    mainWindow.loadURL(`file://${__dirname}/public/index.html`);
-
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-    });
-}
 let connector = null;
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', (err) => {
-    createWindow();
+    const menu = defaultMenu(app, shell);
 
-    // pairing(mainWindow, role);
+    menu.splice(1, 0, {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Open Video Window',
+                click: (item, focusedWindow) => {
+                    if(!role.role && !mainWindow) {
+                        mainWindow = window_builder.createMainWindow(role, windowCloser);
+                    }
+                }
+            },
+            {
+                label: 'New Share Window',
+                click: (item, focusedWindow) => {
+                    if(!role.role) {
+                        shareWindow.createParentwindow(`file://${__dirname}/public/test.html`, new Date().getTime(), connector);
+                    }
+                }
+            },
+
+        ]
+    });
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
+
+    mainWindow = window_builder.createMainWindow(windowCloser);
+
     connector = new Connector(mainWindow, parentWindows, childWindows, role, shareWindow);
+
 });
 
 // Quit when all windows are closed.
@@ -66,9 +72,21 @@ app.on('activate', function() {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
-        createWindow();
+        if (role.role === null) {
+            mainWindow = window_builder.createMainWindow(windowCloser);
+        } else {
+            mainWindow = window_builder.createVideoWindow(role, windowCloser);
+        }
     }
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+/* mainWindowが閉じたときのコールバック */
+function windowCloser() {
+    mainWindow = null;
+    if(!role.role) {
+        mainWindow = window_builder.createVideoWindow(role, windowCloser);
+    }
+}
