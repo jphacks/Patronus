06517e900c1d12d48ea39667867283fb52eeb061
大部達也 <tobe@toralab.org>
2016-11-14 15:40:18 +0900

    reindent and remove unused codes

--- server/index.js
+++ server/index.js
@@ -33,26 +33,23 @@ io.sockets.on("connection", (socket) => {
     });
 
     socket.on('createShareWindow', (data) => {
-        console.log(data)
-    io.emit('createShareWindow', data);
+        console.log('create_window: ', data);
+        io.emit('createShareWindow', data);
     });
     socket.on('closeShareWindow', (data) => {
-    io.emit('closeShareWindow', data);
+        io.emit('closeShareWindow', data);
     });
     socket.on('move', (data) => {
-    io.emit('move', data);
+        io.emit('move', data);
     });
     socket.on('resize', (data) => {
-    io.emit('resize', data);
+        io.emit('resize', data);
     });
     socket.on('updated', (data) => {
-    io.emit('updated', data);
+        io.emit('updated', data);
     });
     socket.on('scroll', (data) => {
-    io.emit('scroll', data);
+        io.emit('scroll', data);
     });
-    socket.on('create_guider_window', (data) => {
-        console.log('create_guider_window', data);
-        socket.to(data.room).emit('create_guider_window', data);
-    });
+
 });
