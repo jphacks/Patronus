49ed3d1be90afa8decc43c79ed54e9d2ce37eaf9
吉田圭佑 <kyoshida@toralab.org>
2016-11-17 21:42:21 +0900

    add global shortcuts

--- application/index.js
+++ application/index.js
@@ -20,7 +20,6 @@ const globalShortcutRegister = {};
 expressModule.createLocalHtmlServer();
 
 
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
@@ -65,8 +64,8 @@ app.on('ready', (err) => {
     });
 
     Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
-
-
+    setTraineeGlobalShortcut();
+    setGuiderGlobalShortcut();
 });
 
 // Quit when all windows are closed.
@@ -103,31 +102,52 @@ function windowCloser() {
     }
 }
 
-function setGlobalShortcut(){
+function setTraineeGlobalShortcut(){
 
-    globalShortcutRegister['up_opacity'] = globalShortcut.register('CommandOrControl+Alt+O',()=>{
+    globalShortcutRegister['O'] = globalShortcut.register('CommandOrControl+Alt+O',()=>{
         mainWindow.webContents.send('change_opacity_up',{});
+        console.log('CommandOrControl+Alt+O');
     });
 
-    globalShortcutRegister['down_opacity'] = globalShortcut.register('CommandOrControl+Alt+Shift+O',()=>{
+    globalShortcutRegister['SO'] = globalShortcut.register('CommandOrControl+Alt+Shift+O',()=>{
         mainWindow.webContents.send('change_opacity_down',{});
+        console.log('CommandOrControl+Alt+Shift+O');
 
     });
 
-    globalShortcutRegister['up_scale'] = globalShortcut.register('CommandOrControl+Alt+L',()=>{
-        mainWindow.webContents.send('remote_scale_up',{});
-
-    });
-
-    globalShortcutRegister['down_scale'] = globalShortcut.register('CommandOrControl+Alt+Shift+L',()=>{
-        mainWindow.webContents.send('remote_scale_down',{});
-
-    });
-
-    globalShortcutRegister['face_mode'] = globalShortcut.register('CommandOrControl+Alt+J',()=>{
+    globalShortcutRegister['J'] = globalShortcut.register('CommandOrControl+Alt+J',()=>{
         mainWindow.webContents.send('change_draw_type',{});
+        console.log('CommandOrControl+Alt+J');
 
     });
 
 }
 
+function setGuiderGlobalShortcut(){
+
+    globalShortcutRegister['L'] = globalShortcut.register('CommandOrControl+Alt+L',()=>{
+        mainWindow.webContents.send('remote_scale_up',{});
+        console.log('CommandOrControl+Alt+L');
+
+    });
+
+    globalShortcutRegister['SL'] = globalShortcut.register('CommandOrControl+Alt+Shift+L',()=>{
+        mainWindow.webContents.send('remote_scale_down',{});
+        console.log('CommandOrControl+Alt+Shift+L');
+
+    });
+
+
+}
+
+
+function clearTraineeGlobalShortcut(){
+    globalShortcut.unregister('CommandOrControl+Alt+O');
+    globalShortcut.unregister('CommandOrControl+Alt+Shift+O');
+    globalShortcut.unregister('CommandOrControl+Alt+J');
+}
+
+function clearGuiderGlobalShortcut(){
+    globalShortcut.unregister('CommandOrControl+Alt+L');
+    globalShortcut.unregister('CommandOrControl+Alt+Shift+L');
+}
